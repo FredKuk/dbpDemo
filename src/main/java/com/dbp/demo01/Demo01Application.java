12c737a718d1b1dfdcfdae08dd37ea8bbec81d9a
souress2 <souress2@naver.com>
2022-11-25 15:30:25 +0900

    jenkinstest5

--- src/main/java/com/dbp/demo01/Demo01Application.java
+++ src/main/java/com/dbp/demo01/Demo01Application.java
@@ -8,5 +8,6 @@ public class Demo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
 		System.out.println("ASDF");
+		System.out.println("ASDF");
 	}
 }
