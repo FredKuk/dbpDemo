f22fc2ef05e9d28cbb37ad1b4b1f0b1550817519
souress2 <souress2@naver.com>
2022-11-25 17:15:14 +0900

    test runner

--- src/main/java/com/dbp/demo01/Demo01Application.java
+++ src/main/java/com/dbp/demo01/Demo01Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
-		System.out.println("ASDF");
-		System.out.println("ASDF");
+		System.out.println("ASDdasdfasfF");
 	}
 }
