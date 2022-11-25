41e9a9fa59b382be723d607663036ebb2dc377f8
souress2 <souress2@naver.com>
2022-11-25 15:01:57 +0900

    jenkinstest2

--- src/main/java/com/dbp/demo01/Demo01Application.java
+++ src/main/java/com/dbp/demo01/Demo01Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo01Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
+		
+		System.out.println("test");
 	}
 }
