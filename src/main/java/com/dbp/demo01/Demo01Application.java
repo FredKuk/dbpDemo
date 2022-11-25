5db4e5b1de6f8758f112c6ddd83de9a7e8a276bd
souress2 <souress2@naver.com>
2022-11-25 18:32:25 +0900

    first

--- src/main/java/com/dbp/demo01/Demo01Application.java
+++ src/main/java/com/dbp/demo01/Demo01Application.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
-		System.out.println("ASDdasdfasfF");
 	}
 }
