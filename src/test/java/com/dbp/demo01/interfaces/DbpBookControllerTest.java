6718ac273bee3f3cd0255c42790cff78e55ede68
souress2 <souress2@naver.com>
2022-11-28 20:05:20 +0900

    junit Test

--- src/test/java/com/dbp/demo01/interfaces/DbpBookControllerTest.java
+++ src/test/java/com/dbp/demo01/interfaces/DbpBookControllerTest.java
@@ -1,31 +1,20 @@
 package com.dbp.demo01.interfaces;
-package kr.co.fastcampus.eatgo.interfaces;
-
-import kr.co.fastcampus.eatgo.application.UserService;
-import kr.co.fastcampus.eatgo.domain.User;
-import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.mockito.internal.matchers.Null;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.dbp.demo01.controller.impl_jaden.DbpBookControllerImpl;
-import com.dbp.demo01.model.Book;
-import com.dbp.demo01.service.BookService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.ArgumentMatchers.eq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dbp.demo01.controller.impl_jaden.DbpBookControllerImpl;
+import com.dbp.demo01.model.Book;
+import com.dbp.demo01.service.BookService;
 
 @WebMvcTest(DbpBookControllerImpl.class)
 public class DbpBookControllerTest {
@@ -39,7 +28,7 @@ public class DbpBookControllerTest {
     @Test
     public void findBookAll() throws Exception {
         List<Book> books = new ArrayList<Book>();
-        given(BDDMockito.willReturn(books));
+        given(booksService.findAll()).willReturn(books);
         mvc.perform(get("/jaden/book"))
             .andExpect(status().isOk());
     }
